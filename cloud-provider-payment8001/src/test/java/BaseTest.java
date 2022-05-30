78ee4644963e565fd6c18eef70dd1757e93ca9e7
hexb3 <hexb3@asiainfo.com>
2022-05-30 19:24:11 +0800

    1.2 创建 cloud-provider-payment8001 微服务提供者Module模块

---
+++ cloud-provider-payment8001/src/test/java/BaseTest.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseTest {
+
+        public static void main(String[] args) {
+            System.out.println("你好");
+        }
+
+}
