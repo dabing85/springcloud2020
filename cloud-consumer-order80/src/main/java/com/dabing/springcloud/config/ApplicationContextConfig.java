386410224ce554c9f5ffe26d4d75cba212d2dded
hexb3 <hexb3@asiainfo.com>
2022-05-30 19:26:35 +0800

    1.3 创建 cloud-consumer-order80 微服务消费者订单Module模块

---
+++ cloud-consumer-order80/src/main/java/com/dabing/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,16 @@
+package com.dabing.springcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationContextConfig {
+
+    //往容器中添加一个RestTemplate
+    //RestTemplate提供了多种便捷访问远程http访问的方法
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
