002b3c5708ca830018b6b515f2b269e943eb44eb
dabing <2261617090@qq.com>
2022-08-04 11:18:42 +0800

    7.1 consul服务消费者

---
+++ cloud-consumer-consul-order80/src/main/java/com/dabing/springcloud/ConsulOrderMain80.java
@@ -0,0 +1,13 @@
+package com.dabing.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient  //该注解用于向使用consul或者Zookeeper作为注册中心时注册服务
+@SpringBootApplication
+public class ConsulOrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulOrderMain80.class);
+    }
+}
