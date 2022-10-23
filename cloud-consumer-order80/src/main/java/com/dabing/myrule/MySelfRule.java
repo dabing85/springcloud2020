6c8c49d86205d32f8f46077066fc103ca3200369
dabing <2261617090@qq.com>
2022-10-23 17:50:13 +0800

    8.2 Ribbon修改负载均衡规则，改为随机

---
+++ cloud-consumer-order80/src/main/java/com/dabing/myrule/MySelfRule.java
@@ -0,0 +1,15 @@
+package com.dabing.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MySelfRule {
+    @Bean
+    public IRule myRule(){
+        return new RandomRule();//使用随机的负载均衡方式
+    }
+
+}
--- cloud-consumer-order80/src/main/java/com/dabing/springcloud/ConsumerMain80.java
+++ cloud-consumer-order80/src/main/java/com/dabing/springcloud/ConsumerMain80.java
@@ -1,11 +1,14 @@
 package com.dabing.springcloud;
 
+import com.dabing.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class ConsumerMain80 {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerMain80.class);
