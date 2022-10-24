c3701f8afe260d180fa074ace84ab0df0d3ceca0
dabing <2261617090@qq.com>
2022-10-24 19:25:31 +0800

    9.4 openFeign日志打印

---
+++ cloud-consumer-feign-order80/src/main/java/com/dabing/springcloud/config/FeignConfig.java
@@ -0,0 +1,14 @@
+package com.dabing.springcloud.config;
+
+import feign.Logger;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootConfiguration
+public class FeignConfig {
+
+    @Bean
+    public Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
+    }
+}
