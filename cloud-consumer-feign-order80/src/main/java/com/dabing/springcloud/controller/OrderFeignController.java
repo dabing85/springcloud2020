ddcfd65e892a6776092e2a91aa782aae69ead3de
dabing <2261617090@qq.com>
2022-10-24 15:26:10 +0800

    9.1 OpenFeign消费者 调用服务

---
+++ cloud-consumer-feign-order80/src/main/java/com/dabing/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,21 @@
+package com.dabing.springcloud.controller;
+
+import com.dabing.springcloud.entities.CommonResult;
+import com.dabing.springcloud.service.PaymentFeignService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderFeignController {
+
+    @Autowired
+    private PaymentFeignService paymentFeignService;  //调用远程的微服接口
+
+    @GetMapping(value = "/consumer/openFeign/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id){
+       return paymentFeignService.getPaymentById(id);
+    }
+
+}
