58ef3ae6aae17b4861b69458ef58cbee9b585cab
dabing <2261617090@qq.com>
2022-10-25 21:28:29 +0800

    10.7 hystrix - 服务熔断

--- cloud-provider-hystrix-payment8001/src/main/java/com/dabing/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/dabing/springcloud/service/PaymentService.java
@@ -3,4 +3,5 @@ package com.dabing.springcloud.service;
 public interface PaymentService {
     public String paymentInfo_OK(Integer id);
     public String payment_Timeout(Integer id);
+    public String paymentCircuitBreaker(Integer id);
 }
