da67c3f326d3e7cd848bea3f620c0f74146249bf
dabing <2261617090@qq.com>
2022-10-25 11:59:47 +0800

    10.1 hystrix - 80消费端压力测试

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/dabing/springcloud/service/PaymentHystrixService.java
@@ -0,0 +1,18 @@
+package com.dabing.springcloud.service;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(value = "cloud-hystrix-payment-service")
+public interface PaymentHystrixService {
+
+    @GetMapping("/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id);
+
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    public String payment_Timeout(@PathVariable("id") Integer id);
+}
