c902082eb6e22df9164d867c3e0f92ad69bf2bb1
dabing <2261617090@qq.com>
2022-10-25 12:14:12 +0800

    10.3 hystrix - 8001fallback

--- cloud-provider-hystrix-payment8001/src/main/java/com/dabing/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/dabing/springcloud/service/impl/PaymentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.dabing.springcloud.service.impl;
 
 import com.dabing.springcloud.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -14,9 +16,21 @@ public class PaymentServiceImpl implements PaymentService {
     }
 
     //失败
+    //超时降级演示
+    @HystrixCommand(fallbackMethod = "payment_TimeoutHandler",commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="5000") //5秒钟以内就是正常的业务逻辑
+    })
     public String payment_Timeout(Integer id){
-        int timeNumber = 3;
+        //int timeNumber = 3; //正常情况
+        int i = 1/0 ; //模拟非正常情况
+        int timeNumber = 10;  //模拟超时情况
         try { TimeUnit.SECONDS.sleep(timeNumber); }catch (Exception e) {e.printStackTrace();}
         return "线程池："+Thread.currentThread().getName()+"   paymentInfo_TimeOut,id：  "+id+"\t"+"呜呜呜"+" 耗时(秒)"+timeNumber;
+    }
+
+    //兜底方法，上面方法出问题,我来处理，返回一个出错信息
+    //返回值和参数要跟原方法一样
+    public String payment_TimeoutHandler(Integer id) {
+        return "线程池："+Thread.currentThread().getName()+" payment_TimeoutHandler,系统繁忙,请稍后再试\t o(╥﹏╥)o ";
     }
 }
