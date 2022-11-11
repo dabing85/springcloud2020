7e6ff3d328dcc785b72bccf8c92888dbd4256351
dabing <2261617090@qq.com>
2022-11-11 13:26:00 +0800

    17.2 - sentinel热点参数限流

--- cloudalibaba-sentinel-service8401/src/main/java/com/dabing/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/dabing/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.dabing.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +16,19 @@ public class FlowLimitController {
     @GetMapping("/testB")
     public String testB() {
         return "------testB";
+    }
+
+    //热点限流测试
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2) {
+        //int age = 10/0;
+        return "------testHotKey";
+    }
+
+    //兜底方法
+    public String deal_testHotKey (String p1, String p2, BlockException exception){
+        return "------deal_testHotKey,o(╥﹏╥)o";
     }
 }
