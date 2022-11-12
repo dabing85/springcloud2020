f39a2d1eaa6bbfb1ee2142d0abbf4e56323a20b5
dabing <2261617090@qq.com>
2022-11-12 10:46:41 +0800

    17.3-@SentinelResource 按url限流+后续处理

--- cloudalibaba-sentinel-service8401/src/main/java/com/dabing/springcloud/alibaba/controller/RateLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/dabing/springcloud/alibaba/controller/RateLimitController.java
@@ -18,4 +18,11 @@ public class RateLimitController {
     public CommonResult handleException(BlockException exception){
         return new CommonResult(444,exception.getClass().getCanonicalName()+"\t 服务不可用");
     }
+
+    @GetMapping("/rateLimit/byUrl")
+    @SentinelResource(value = "byUrl")
+    public CommonResult byUrl(){
+        return new CommonResult(200,"按url限流测试OK",new Payment(2020L,"serial002"));
+    }
+
 }
